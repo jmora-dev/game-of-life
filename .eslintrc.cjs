471c298cdc58c127d762d83343e9d411160e34ca
jmora-dev <dam2mora@gmail.com>
2022-05-19 19:15:34 +0200

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+		jest: true,
+	},
+	extends: ['eslint:recommended', 'prettier'],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	rules: {
+		indent: ['error', 'tab'],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'always'],
+	},
+};
