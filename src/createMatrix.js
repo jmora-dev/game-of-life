84a3ef9185a0646b474812bfde93d4ffc9499bb3
jmora-dev <dam2mora@gmail.com>
2022-05-20 07:10:03 +0200

    Fix createMatrix function

--- src/createMatrix.js
+++ src/createMatrix.js
@@ -2,5 +2,13 @@ export const createMatrix = (height, width) => {
 	if (!Number.isInteger(height) || !Number.isInteger(width)) {
 		throw new TypeError('Parameters must be integer');
 	}
-	return new Array(height).fill(new Array(width).fill(false));
+	const matrix = [];
+	for (let y = 0; y < height; y++) {
+		const row = [];
+		for (let x = 0; x < width; x++) {
+			row.push(false);
+		}
+		matrix.push(row);
+	}
+	return matrix;
 };
